e1657ae35a74a987a782c15289acb3e0f1b612ee
Mateusz Serwacinski <serwaaagh@gmail.com>
2023-04-30 16:18:02 +0200

    2nd test

--- src/test/java/UltimateQATest.java
+++ src/test/java/UltimateQATest.java
@@ -9,7 +9,20 @@ public class UltimateQATest extends PageSetup{
     @Test
     public void testone(){
         driver.findElementById( "idExample").click();
-        WebElement ButtonSuccess = driver.findElementByClassName("entry-title");
-        Assertions.assertEquals("Button success", ButtonSuccess.getText());
+            WebElement ButtonSuccess = driver.findElementByClassName("entry-title");
+            Assertions.assertEquals("Button success", ButtonSuccess.getText());
+    }
+    @Test
+    public void testTwo() throws InterruptedException {
+        WebElement nameInput = driver.findElementById("et_pb_contact_name_0");
+            nameInput.sendKeys("Tester");
+        WebElement emailInput = driver.findElementById("et_pb_contact_email_0");
+            emailInput.sendKeys("tester@test.com");
+            Thread.sleep(2000);
+        WebElement emailMeButton = driver.findElementByName("et_builder_submit_button");
+        emailMeButton.click();
+            Thread.sleep(2000);
+        WebElement thanksText = driver.findElementByXPath("//div[@class='et-pb-contact-message']/p");
+            Assertions.assertEquals("Thanks for contacting us", thanksText.getText());
     }
 }
